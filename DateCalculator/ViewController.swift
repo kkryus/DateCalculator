70738a7de337d9ac6a2b3ee15e6f12f9e3803d3e
RMS2018 <rms2018@macbook-pro-zdzichs.local>
2018-10-25 16:15:31 +0200

    Initial Commit

---
+++ DateCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DateCalculator
+//
+//  Created by RMS2018 on 25.10.2018.
+//  Copyright © 2018 RMS2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
