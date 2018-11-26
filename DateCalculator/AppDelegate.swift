f2d27186606d0f9e8cfdb6b98fc8c5f2ac392dce
WTF <wtf@WTFs-iMac.local>
2018-11-26 03:07:17 -0800

    fxcode

--- DateCalculator/AppDelegate.swift
+++ DateCalculator/AppDelegate.swift
@@ -13,8 +13,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //home
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        if let launchOptions = launchOptions {
+            if #available(iOS 9.0, *) {
+                if let shortcutItem = launchOptions[UIApplicationLaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+                    print("Shortcut: \(shortcutItem)")
+                }
+            }
+        }
+        return true
+    }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    //school
+    /*private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if let launchOptions = launchOptions {
             if #available(iOS 9.0, *) {
                 if let shortcutItem = launchOptions[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
@@ -23,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         return true
-    }
+    }*/
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- DateCalculator/ViewController.swift
+++ DateCalculator/ViewController.swift
@@ -7,21 +7,63 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var dateInputTextBox: UITextField!
+    @IBOutlet weak var countryInputTextBox: UITextField!
+    @IBOutlet weak var calendarOutputTextBox: UITextField!
+    
+    var dateFormat: String = "dd.mm.yyyy"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func aqwew(_ sender: UITextField) {
-        aaa.text = "foooooo"
-    }
-    
-    @IBOutlet weak var aaa: UITextField!
-    @IBAction func avc(_ sender: Any) {
+    @IBAction func startButton_OnTouchUp(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        let date = dateFormatter.date(from: dateInputTextBox.text!)
+        calendarOutputTextBox.text = dateFormat//dateFormatter.string(from: date!)//inputDateTextBox.text
+
         
+    }
+    @IBAction func dateInput_OnValueChanged(_ sender: Any) {
+        if dateInputTextBox.text!.range(of:".") != nil {
+            calendarOutputTextBox.text = "kropka"
+            dateFormat = "dd.mm.yyyy"
+        }
+        else if dateInputTextBox.text!.range(of:"/") != nil{
+            calendarOutputTextBox.text = "2"
+            dateFormat = "dd/mm/yyyy"
+        }
+        else if dateInputTextBox.text!.range(of:"-") != nil {
+            calendarOutputTextBox.text = "3"
+            dateFormat = "dd-mm-yyyy"
+        }
+        calendarOutputTextBox.text = "4"
+        /*if dateInputTextBox.text!.range(of:".") != nil {
+            dateFormat = "dd.mm.yyyy"
+        }
+        else if dateInputTextBox.text!.range(of:"/") != nil{
+            dateFormat = "dd/mm/yyyy"
+        }
+        else if dateInputTextBox.text!.range(of:"-") != nil {
+            dateFormat = "dd-mm-yyyy"
+        }*/
+    }
+    @IBAction func dateInput_OnEditingEnd(_ sender: Any) {
+        /*if dateInputTextBox.text!.range(of:".") != nil {
+            dateFormat = "dd.mm.yyyy"
+        }
+        else if dateInputTextBox.text!.range(of:"/") != nil{
+            dateFormat = "dd/mm/yyyy"
+        }
+        else if dateInputTextBox.text!.range(of:"-") != nil {
+            dateFormat = "dd-mm-yyyy"
+        }*/
     }
 }
 
