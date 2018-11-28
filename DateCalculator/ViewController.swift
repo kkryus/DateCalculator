6f267b7bd87f2554b74733ebf1e0763fdfd3a3d3
WTF <wtf@WTFs-iMac.local>
2018-11-28 05:51:57 -0800

    been days implemented

--- DateCalculator/ViewController.swift
+++ DateCalculator/ViewController.swift
@@ -14,8 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var dateInputTextBox: UITextField!
     @IBOutlet weak var countryInputTextBox: UITextField!
     @IBOutlet weak var calendarOutputTextBox: UITextField!
+    @IBOutlet weak var daysSinceNowTextBo: UITextField!
+    @IBOutlet weak var monthsSinceNowTextBox: UITextField!
+    @IBOutlet weak var easterDateTextBox: UITextField!
+    @IBOutlet weak var dayOfTheWeekTextBox: UITextField!
+    @IBOutlet weak var beenYearsTextBox: UITextField!
+    @IBOutlet weak var beenMonthsTextBox: UITextField!
+    @IBOutlet weak var beenDaysTextBox: UITextField!
     
-    var dateFormat: String = "dd.mm.yyyy"
+    
+    var dateFormat: String = "dd.MM.yyyy"
+    var country: String = "Poland"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,47 +32,59 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startButton_OnTouchUp(_ sender: Any) {
+        setDateFormat()
+        setCountry()
+        countDaysSinceDate()
+        //calendarOutputTextBox.text = dateFormat
+       // let dateFormatter = DateFormatter()
+        //dateFormatter.dateFormat = dateFormat
+       // let date = dateFormatter.date(from: dateInputTextBox.text!)
+        //calendarOutputTextBox.text = dateFormatter.string(from: date!)
+    }
+    private func countDaysSinceDate(){
+        let calendar = Calendar.current
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = dateFormat
         let date = dateFormatter.date(from: dateInputTextBox.text!)
-        calendarOutputTextBox.text = dateFormat//dateFormatter.string(from: date!)//inputDateTextBox.text
-
         
+        // Replace the hour (time) of both dates with 00:00
+        let date1 = calendar.startOfDay(for: date!)
+        let date2 = calendar.startOfDay(for: Date())
+        
+        let components = calendar.dateComponents([.day, .month, .year], from: date1, to: date2)
+        beenDaysTextBox.text = String(describing: components.day!)
+        beenMonthsTextBox.text = String(describing: components.month!)
+        beenYearsTextBox.text = String(describing: components.year!)
     }
-    @IBAction func dateInput_OnValueChanged(_ sender: Any) {
-        if dateInputTextBox.text!.range(of:".") != nil {
-            calendarOutputTextBox.text = "kropka"
-            dateFormat = "dd.mm.yyyy"
-        }
-        else if dateInputTextBox.text!.range(of:"/") != nil{
-            calendarOutputTextBox.text = "2"
-            dateFormat = "dd/mm/yyyy"
+    
+    private func setDateFormat(){
+        if dateInputTextBox.text!.range(of:"/") != nil{
+            dateFormat = "dd/MM/yyyy"
+            return
         }
         else if dateInputTextBox.text!.range(of:"-") != nil {
-            calendarOutputTextBox.text = "3"
-            dateFormat = "dd-mm-yyyy"
+            dateFormat = "dd-MM-yyyy"
+            return
         }
-        calendarOutputTextBox.text = "4"
-        /*if dateInputTextBox.text!.range(of:".") != nil {
-            dateFormat = "dd.mm.yyyy"
-        }
-        else if dateInputTextBox.text!.range(of:"/") != nil{
-            dateFormat = "dd/mm/yyyy"
-        }
-        else if dateInputTextBox.text!.range(of:"-") != nil {
-            dateFormat = "dd-mm-yyyy"
-        }*/
+        dateFormat = "dd.MM.yyyy"
     }
-    @IBAction func dateInput_OnEditingEnd(_ sender: Any) {
-        /*if dateInputTextBox.text!.range(of:".") != nil {
-            dateFormat = "dd.mm.yyyy"
+    
+    private func setCountry(){
+        if countryInputTextBox.text!.range(of:"Hungary") != nil {
+            country = "Hungary"
+            return
         }
-        else if dateInputTextBox.text!.range(of:"/") != nil{
-            dateFormat = "dd/mm/yyyy"
+        else if countryInputTextBox.text!.range(of:"Spain") != nil{
+            country = "Spain"
+            return
         }
-        else if dateInputTextBox.text!.range(of:"-") != nil {
-            dateFormat = "dd-mm-yyyy"
-        }*/
+        else if countryInputTextBox.text!.range(of:"Great Britain") != nil {
+            country = "Great Britain"
+            return
+        }
+        country = "Poland"
+        
     }
 }
 
