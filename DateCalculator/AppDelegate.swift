b3b38d7528d2fc4af206c04aa3d00cfb00995ef4
RMS2018 <rms2018@macbook-pro-zdzichs.local>
2018-12-06 15:58:47 +0100

    school ver

--- DateCalculator/AppDelegate.swift
+++ DateCalculator/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     //home
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    /*func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         if let launchOptions = launchOptions {
             if #available(iOS 9.0, *) {
                 if let shortcutItem = launchOptions[UIApplicationLaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
@@ -23,10 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         return true
-    }
+    }*/
 
     //school
-    /*private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if let launchOptions = launchOptions {
             if #available(iOS 9.0, *) {
                 if let shortcutItem = launchOptions[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         return true
-    }*/
+    }//*/
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
